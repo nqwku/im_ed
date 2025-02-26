db6ab487357a584b84bb3ce7fa0a2e0ae2aacd36
nqwku <naquewuk89@gmail.com>
2025-02-26 12:20:58 +0200

    Added setvbuf for groupiing IO operations, also block processing for grayscale was implemented(defined BLOCK_SIZE, CACHE_LINE, CACHE_BLOCK_SIZE), also added SIMD optimization for x86_64 with SSE instructions, implemented schedule(guided) for multithreading planning

--- src/main.c
+++ src/main.c
@@ -5,6 +5,10 @@
 #include <math.h>
 #include <ctype.h>
 
+#ifdef __x86_64__
+#include <immintrin.h>
+#endif
+
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
@@ -13,6 +17,11 @@
 
 #define JPEG_QUALITY 90
 #define CLAMP(x) (((x) > 255) ? 255 : ((x) < 0) ? 0 : (x))
+
+#define BLOCK_SIZE 64
+#define CACHE_LINE 64
+
+#define CACHE_BLOCK_SIZE 32
 
 int use_thread = 1;
 
@@ -28,6 +37,8 @@ void invert(unsigned char *image, int width, int height, int channels, float par
 void brightness(unsigned char *image, int width, int height, int channels, float brightness);
 void contrast(unsigned char *image, int width, int height, int channels, float factor);
 void sepia(unsigned char *image, int width, int height, int channels, float param);
+
+void grayscale_simd(unsigned char *image, int width, int height, int channels, float param);
 
 typedef struct {
     const char *name;
@@ -46,8 +57,8 @@ Filter filter[] = {
 const int num_filters = sizeof(filter) / sizeof(Filter);
 
 int main(int argc, char *argv[]) {
-
     printf("argc: %d\n%s\n", argc, file_format(argv[1]));
+
     printf("Processing with %d threads\n", omp_get_max_threads());
 
     int num_threads = omp_get_num_procs();
@@ -65,6 +76,14 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "Error: only .png & .jpg files support");
         return 1;
     }
+
+    FILE* input_file = fopen(argv[1], "rb");
+    if (!input_file) {
+        fprintf(stderr, "Error: could not open file %s\n", argv[1]);
+        return 1;
+    }
+
+    setvbuf(input_file, NULL, _IOFBF, 1024 * 1024);
 
     int width, height, channels;
     unsigned char *image = stbi_load(argv[1], &width, &height, &channels, 0);
@@ -87,6 +106,16 @@ int main(int argc, char *argv[]) {
         }
     }
 
+    unsigned char *image_copy = NULL;
+    if (benchmark_mode) {
+        image_copy = (unsigned char *)malloc(width * height * channels);
+        if (!image_copy) {
+            fprintf(stderr, "Error: failed to allocate memory for image benchmark\n");
+            stbi_image_free(image_copy);
+            return 1;
+        }
+    }
+
     for(int i = 3; i < argc; i++) {
         for (int j = 0; j < num_filters; j++) {
             if (strcmp(argv[i], filter[j].name) == 0) {
@@ -95,18 +124,13 @@ int main(int argc, char *argv[]) {
                     if (i + 1 >= argc || !is_number(argv[i + 1])) {
                         fprintf(stderr, "Usage: %s [param value]\n", filter[j].name);
                         stbi_image_free(image);
+                        if (image_copy) free(image_copy);
                         return 1;
                     }
                     param = tmp_atof(argv[i + 1]);
                     i++;
                 }
                 if (benchmark_mode) {
-                    unsigned char * image_copy = malloc(width * height * channels);
-                    if (!image_copy) {
-                        fprintf(stderr, "Error: failed to allocate memory for image benchmark\n");
-                        continue;
-                    }
-
                     memcpy(image_copy, image, width * height * channels);
 
                     use_thread = 1;
@@ -132,23 +156,30 @@ int main(int argc, char *argv[]) {
     }
 
     const char *ext = strrchr(argv[2], '.');
-    if (ext !=NULL) {
+    if (ext != NULL) {
         if (strstr(ext, ".jpg") || strstr(ext, ".jpeg")) {
             if (!stbi_write_jpg(argv[2], width, height, channels, image, JPEG_QUALITY)) {
                 fprintf(stderr, "Failed to write JPEG.\n");
+                stbi_image_free(image);
+                if (image_copy) free(image_copy);
                 return 1;
             }
         } else if (strstr(ext, ".png")) {
             if(!stbi_write_png(argv[2], width, height, channels, image, width * channels)) {
                 fprintf(stderr, "Failed to write PNG.\n");
+                stbi_image_free(image);
+                if (image_copy) free(image_copy);
                 return 1;
             }
         }
     } else {
         fprintf(stderr, "Failed to open: no extension \n");
+        stbi_image_free(image);
+        if (image_copy) free(image_copy);
         return 1;
     }
     stbi_image_free(image);
+    if (image_copy) free(image_copy);
     return 0;
 }
 
@@ -231,48 +262,60 @@ double filter_time(void (*func)(unsigned char*, int, int, int, float),
 
 
 void grayscale(unsigned char *image, int width, int height, int channels, float param) {
+
+    const float r_factor = 0.298f;
+    const float g_factor = 0.587f;
+    const float b_factor = 0.114f;
+
+    const int total_pixels = width * height;
+
     if (use_thread){
-#pragma omp parallel for
-        for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++) {
-                int index = (y * width + x) * channels;
-                float gray = 0.298f * image[index + 0] + 0.587f * image[index + 1] + 0.114f * image[index + 2];
-                image[index + 0] = gray;
-                image[index + 1] = gray;
-                image[index + 2] = gray;
+#pragma omp parallel for schedule(guided)
+        for (int block_y = 0; block_y < height; block_y += CACHE_BLOCK_SIZE) {
+            for (int block_x = 0; block_x < width; block_x += CACHE_BLOCK_SIZE) {
+                const int max_y = (block_y + CACHE_BLOCK_SIZE < height) ? block_y + CACHE_BLOCK_SIZE : height;
+                const int max_x = (block_x + CACHE_BLOCK_SIZE < width) ? block_x + CACHE_BLOCK_SIZE : width;
+
+                for (int y = block_y; y < max_y; y++) {
+                    for (int x = block_x; x < max_x; x++) {
+                        const int idx = (y * width + x) * channels;
+                        const float gray = r_factor * image[idx] + g_factor * image[idx + 1] + b_factor * image[idx + 2];
+                        const unsigned char gray_byte = (unsigned char)gray;
+
+                        image[idx] = gray_byte;
+                        image[idx + 1] = gray_byte;
+                        image[idx + 2] = gray_byte;
+                    }
+                }
             }
         }
     } else {
-        for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++) {
-                int index = (y * width + x) * channels;
-                float gray = 0.298f * image[index + 0] + 0.587f * image[index + 1] + 0.114f * image[index + 2];
-                image[index + 0] = gray;
-                image[index + 1] = gray;
-                image[index + 2] = gray;
-            }
+        for (int i = 0; i < total_pixels; i++) {
+            const int idx = i * channels;
+            const float gray = r_factor * image[idx] + g_factor * image[idx + 1] + b_factor * image[idx + 2];
+            const unsigned char gray_byte = (unsigned char)gray;
+
+            image[idx] = gray_byte;
+            image[idx + 1] = gray_byte;
+            image[idx + 2] = gray_byte;
         }
     }
 }
 
 void invert(unsigned char *image, int width, int height, int channels, float param) {
+    const int total_size = width * height * channels;
+
     if (use_thread){
-#pragma omp parallel for
-        for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++) {
-                int index = (y * width + x) * channels;
-                image[index + 0] = 255 - image[index + 0];
-                image[index + 1] = 255 - image[index + 1];
-                image[index + 2] = 255 - image[index + 2];
+#pragma omp parallel for schedule(guided)
+        for (int i = 0; i < total_size; i += channels) {
+            for (int c = 0; c < 3 && c < channels; c++) {
+                image[i + c] = 255 - image[i + c];
             }
         }
     } else {
-        for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++) {
-                int index = (y * width + x) * channels;
-                image[index + 0] = 255 - image[index + 0];
-                image[index + 1] = 255 - image[index + 1];
-                image[index + 2] = 255 - image[index + 2];
+        for (int i = 0; i < total_size; i += channels) {
+            for (int c = 0; c < 3 && c < channels; c++) {
+                image[i + c] = 255 - image[i + c];
             }
         }
     }
@@ -284,26 +327,18 @@ void brightness(unsigned char *image, int width, int height, int channels, float
         return;
     }
 
-    if (use_thread){
-#pragma omp parallel for
-        for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++) {
-                int index = (y * width + x) * channels;
-                for (int c = 0; c < 3; c++) {
-                    float new_val = image[index + c] * brightness;
-                    image[index + c] = (new_val > 255) ? 255 : (unsigned char)new_val;
-                }
-            }
+    const int total_size = width * height * channels;
+
+    if (use_thread) {
+#pragma omp parallel for schedule(guided)
+        for (int i = 0; i < total_size; i++) {
+            float new_val = image[i] * brightness;
+            image[i] = (new_val > 255.0f) ? 255 : (unsigned char)new_val;
         }
     } else {
-        for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++) {
-                int index = (y * width + x) * channels;
-                for (int c = 0; c < 3; c++) {
-                    float new_val = image[index + c] * brightness;
-                    image[index + c] = (new_val > 255) ? 255 : (unsigned char)new_val;
-                }
-            }
+        for (int i = 0; i < total_size; i++) {
+            float new_val = image[i] * brightness;
+            image[i] = (new_val > 255.0f) ? 255 : (unsigned char)new_val;
         }
     }
 };
@@ -315,7 +350,7 @@ void contrast(unsigned char *image, int width, int height, int channels, float f
     }
 
     if (use_thread) {
-#pragma omp parallel for
+#pragma omp parallel for schedule(guided)
         for (int i = 0; i < width * height * channels; i++) {
             int tmp_image = (int)image[i];
             tmp_image = CLAMP(factor * (tmp_image - 128) + 128);
@@ -332,44 +367,162 @@ void contrast(unsigned char *image, int width, int height, int channels, float f
 
 void sepia(unsigned char *image, int width, int height, int channels, float param) {
     if (channels != 3 && channels != 4) {
-        fprintf(stderr, "Error: Sepia filter requires be 3 or 4 channels.\n");
+        fprintf(stderr, "Error: Sepia filter requires 3 or 4 channels.\n");
         return;
     }
 
-    static const int c_red[3] = {393, 769, 189};
-    static const int c_green[3] = {349, 686, 168};
-    static const int c_blue[3] = {272, 534, 131};
+    static const float c_red[3] = {0.393f, 0.769f, 0.189f};
+    static const float c_green[3] = {0.349f, 0.686f, 0.168f};
+    static const float c_blue[3] = {0.272f, 0.534f, 0.131f};
 
-    if (use_thread){
-#pragma omp parallel for
-        for (size_t i = 0; i < width * height * channels; i += channels) {
-            const int r = image[i];
-            const int g = image[i + 1];
-            const int b = image[i + 2];
+    const int total_pixels = width * height;
 
-            const int sepia_red   = CLAMP((r * c_red[0] + g * c_red[1] + b * c_red[2] )  / 1000);
-            const int sepia_green = CLAMP((r * c_green[0] + g * c_green[1] + b * c_green[2] )  / 1000);
-            const int sepia_blue  = CLAMP((r * c_blue[0] + g * c_blue[1] + b * c_blue[2] )  / 1000);
+    if (use_thread) {
+        #pragma omp parallel for schedule(guided)
+        for (int i = 0; i < total_pixels; i++) {
+            const int idx = i * channels;
+            const int r = image[idx];
+            const int g = image[idx + 1];
+            const int b = image[idx + 2];
 
-            image[i] = sepia_red;
-            image[i + 1] = sepia_green;
-            image[i + 2] = sepia_blue;
+            const int sepia_red   = CLAMP((r * c_red[0] + g * c_red[1] + b * c_red[2]) * 1000.0f);
+            const int sepia_green = CLAMP((r * c_green[0] + g * c_green[1] + b * c_green[2]) * 1000.0f);
+            const int sepia_blue  = CLAMP((r * c_blue[0] + g * c_blue[1] + b * c_blue[2]) * 1000.0f);
+
+            image[idx] = sepia_red;
+            image[idx + 1] = sepia_green;
+            image[idx + 2] = sepia_blue;
         }
-    }
-    else {
-        for (size_t i = 0; i < width * height * channels; i += channels) {
-            const int r = image[i];
-            const int g = image[i + 1];
-            const int b = image[i + 2];
+    } else {
+        for (int i = 0; i < total_pixels; i++) {
+            const int idx = i * channels;
+            const int r = image[idx];
+            const int g = image[idx + 1];
+            const int b = image[idx + 2];
 
-            const int sepia_red   = CLAMP((r * c_red[0] + g * c_red[1] + b * c_red[2] )  / 1000);
-            const int sepia_green = CLAMP((r * c_green[0] + g * c_green[1] + b * c_green[2] )  / 1000);
-            const int sepia_blue  = CLAMP((r * c_blue[0] + g * c_blue[1] + b * c_blue[2] )  / 1000);
+            const int sepia_red   = CLAMP((r * c_red[0] + g * c_red[1] + b * c_red[2]) * 1000.0f);
+            const int sepia_green = CLAMP((r * c_green[0] + g * c_green[1] + b * c_green[2]) * 1000.0f);
+            const int sepia_blue  = CLAMP((r * c_blue[0] + g * c_blue[1] + b * c_blue[2]) * 1000.0f);
 
-            image[i] = sepia_red;
-            image[i + 1] = sepia_green;
-            image[i + 2] = sepia_blue;
+            image[idx] = sepia_red;
+            image[idx + 1] = sepia_green;
+            image[idx + 2] = sepia_blue;
         }
     }
 }
+#ifdef __x86_64__
+void grayscale_simd(unsigned char *image, int width, int height, int channels, float param) {
+    if(channels != 3 && channels != 4) {
+        grayscale(image, width, height, channels, param);
+        return;
+    }
 
+    __m128 r_weight = _mm_set1_ps(0.298f);
+    __m128 g_weight = _mm_set1_ps(0.587f);
+    __m128 b_weight = _mm_set1_ps(0.114f);
+
+    if (use_thread) {
+#pragma omp parallel for schedule(guided)
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x += 4) {
+                int idx = (y * width + x) * channels;
+
+                __m128 r_vals = _mm_set_ps(
+                    image[idx + 0], image[idx + channels + 0],
+                    image[idx + 2 * channels + 0], image[idx + 3 * channels + 0]
+                );
+
+                __m128 g_vals = _mm_set_ps(
+                    image[idx + 1], image[idx + channels + 1],
+                    image[idx + 2 * channels + 1], image[idx + 3 * channels + 1]
+                );
+
+                __m128 b_vals = _mm_set_ps(
+                    image[idx + 2], image[idx + channels + 2],
+                    image[idx + 2 * channels + 2], image[idx + 3 * channels + 2]
+                );
+
+                __m128 gray_vals = _mm_add_ps(
+                    _mm_add_ps(
+                        _mm_mul_ps(r_vals, r_weight),
+                        _mm_mul_ps(g_vals, g_weight)
+                    ),
+                    _mm_mul_ps(b_vals, b_weight)
+                );
+
+                float gray_array[4];
+                _mm_storeu_ps(gray_array, gray_vals);
+
+                for (int i = 0; i < 4; i++) {
+                    int pixel_idx = idx + i * channels;
+                    unsigned char gray_byte = (unsigned char)gray_array[i];
+                    image[pixel_idx + 0] = gray_byte;
+                    image[pixel_idx + 1] = gray_byte;
+                    image[pixel_idx + 2] = gray_byte;
+
+                }
+            }
+
+            for (int x = (width / 4) * 4; x < width; x++) {
+                int idx = (y * width + x) * channels;
+                float gray = 0.298f * image[idx + 0] + 0.587f * image[idx + 1] + 0.114f * image[idx + 2];
+                unsigned char gray_byte = (unsigned char)gray;
+                image[idx + 0] = gray_byte;
+                image[idx + 1] = gray_byte;
+                image[idx + 2] = gray_byte;
+            }
+        }
+    } else {
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x += 4) {
+                int idx = (y * width + x) * channels;
+
+                __m128 r_vals = _mm_set_ps(
+                    image[idx + 0], image[idx + channels + 0],
+                    image[idx + 2 * channels + 0], image[idx + 3 * channels + 0]
+                );
+
+                __m128 g_vals = _mm_set_ps(
+                    image[idx + 1], image[idx + channels + 1],
+                    image[idx + 2 * channels + 1], image[idx + 3 * channels + 1]
+                );
+
+                __m128 b_vals = _mm_set_ps(
+                    image[idx + 2], image[idx + channels + 2],
+                    image[idx + 2 * channels + 2], image[idx + 3 * channels + 2]
+                );
+
+                __m128 gray_vals = _mm_add_ps(
+                    _mm_add_ps(
+                        _mm_mul_ps(r_vals, r_weight),
+                        _mm_mul_ps(g_vals, g_weight)
+                    ),
+                    _mm_mul_ps(b_vals, b_weight)
+                );
+
+                float gray_array[4];
+                _mm_storeu_ps(gray_array, gray_vals);
+
+                for (int i = 0; i < 4; i++) {
+                    int pixel_idx = idx + i * channels;
+                    unsigned char gray_byte = (unsigned char)gray_array[i];
+                    image[pixel_idx + 0] = gray_byte;
+                    image[pixel_idx + 1] = gray_byte;
+                    image[pixel_idx + 2] = gray_byte;
+
+                }
+            }
+
+            for (int x = (width / 4) * 4; x < width; x++) {
+                int idx = (y * width + x) * channels;
+                float gray = 0.298f * image[idx + 0] + 0.587f * image[idx + 1] + 0.114f * image[idx + 2];
+                unsigned char gray_byte = (unsigned char)gray;
+                image[idx + 0] = gray_byte;
+                image[idx + 1] = gray_byte;
+                image[idx + 2] = gray_byte;
+            }
+        }
+    }
+
+}
+#endif
